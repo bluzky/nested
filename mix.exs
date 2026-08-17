522e4af86fc368d3248613c1ad522cb87346ff5c
Always blue <bluesky.1289@gmail.com>
2026-08-17 16:10:55 +0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nested.MixProject do
   def project do
     [
       app: :nestex,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
